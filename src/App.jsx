888512544dd4c8a63c63e4e9373f1c9f314c62e5
Ninz11 <148561307+Nigestyh@users.noreply.github.com>
2025-10-28 15:59:52 +0000

    Add contact state with sample data

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,13 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [contact, setContact] = useState({
+    firstName: "Nigesty",
+    lastName: "Hagos",
+    phone: "+44-745-559-7333",
+    email: "tsmyrealname@gmail.com",
+    isFavourite: false,
+  });
   return (
     <main>
       <h2>Nigesty Hagos</h2>
